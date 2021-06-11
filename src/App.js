9da8648d92011e9457a080cfb234aaae498dc2c9
zaryab-ali <85732412+zaryab-ali@users.noreply.github.com>
2021-06-11 13:05:42 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddInventory from './components/classComponent';
+import AddProduct from './components/funcComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>inventory for cash register</h1>
+    
+        <AddInventory/>
+        <AddProduct/>
+        {/* <h1>We will cover here:</h1>
+        <ul>
+          <li>Reactjs ref vs syntheticEvent</li>
+          <li>Form in reactjs</li>
+          <li>Reactjs Hook: useState()</li>
+          <li>React components: functional vs class</li>
+          <li>Add product name, price, quantity in inventory data</li>
+          <li>React-Bootstrap use case</li>
+        </ul> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
